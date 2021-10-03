1b462d8507fd65fe2e9bd66533c9d36edf0a23e5
Rafik99 <33373189+Rafik99@users.noreply.github.com>
2021-10-03 19:01:09 +0300

    Update song.py

--- DaisyXMusic/modules/song.py
+++ DaisyXMusic/modules/song.py
@@ -50,8 +50,7 @@ def song(client, message):
     for i in message.command[1:]:
         query += " " + str(i)
     print(query)
-    m = message.reply("🔎 Пошук пісні 
-                      ...")
+    m = message.reply("🔎 Пошук пісні...")
     ydl_opts = {"format": "bestaudio/best"}
     try:
         results = YoutubeSearch(query, max_results=1).to_dict()
