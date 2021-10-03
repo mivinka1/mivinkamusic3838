719e3b874da4c5cca230f01ddae2d861e5777b58
Rafik99 <33373189+Rafik99@users.noreply.github.com>
2021-10-03 07:28:38 +0300

    Update ytsearch.py

--- DaisyXMusic/modules/ytsearch.py
+++ DaisyXMusic/modules/ytsearch.py
@@ -36,18 +36,18 @@ logging.getLogger("pyrogram").setLevel(logging.WARNING)
 async def ytsearch(_, message: Message):
     try:
         if len(message.command) < 2:
-            await message.reply_text("/search needs an argument!")
+            await message.reply_text("/search потрібен аргумент!")
             return
         query = message.text.split(None, 1)[1]
-        m = await message.reply_text("Searching....")
+        m = await message.reply_text("Пошук....")
         results = YoutubeSearch(query, max_results=4).to_dict()
         i = 0
         text = ""
         while i < 4:
-            text += f"Title - {results[i]['title']}\n"
-            text += f"Duration - {results[i]['duration']}\n"
-            text += f"Views - {results[i]['views']}\n"
-            text += f"Channel - {results[i]['channel']}\n"
+            text += f"Назва - {results[i]['title']}\n"
+            text += f"Тривалість - {results[i]['duration']}\n"
+            text += f"Переглядів - {results[i]['views']}\n"
+            text += f"Канал - {results[i]['channel']}\n"
             text += f"https://youtube.com{results[i]['url_suffix']}\n\n"
             i += 1
         await m.edit(text, disable_web_page_preview=True)
